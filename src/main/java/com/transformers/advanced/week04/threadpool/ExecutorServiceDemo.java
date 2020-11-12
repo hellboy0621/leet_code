cce2d2c75b5c984a5584a2d8416343cb1682bf50
fanjinpeng <hero0621@126.com>
2020-11-12 17:20:09 +0800

    [java_advanced]awaitTermination需要结合shutdown或shutdownNow方法使用

--- src/main/java/com/transformers/advanced/week04/threadpool/ExecutorServiceDemo.java
+++ src/main/java/com/transformers/advanced/week04/threadpool/ExecutorServiceDemo.java
@@ -1,6 +1,5 @@
 package com.transformers.advanced.week04.threadpool;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -18,10 +17,36 @@ public class ExecutorServiceDemo {
         });
         System.out.println("ExecutorServiceDemo.main");
         try {
+//            executorService.shutdown();
             executorService.shutdownNow();
-            executorService.awaitTermination(1, TimeUnit.MILLISECONDS);
+            System.out.println(executorService.awaitTermination(1, TimeUnit.MILLISECONDS));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+    /**
+     * awaitTermination需要结合shutdown或shutdownNow方法使用
+     *
+     * executorService.shutdown();
+     *
+     * ExecutorServiceDemo.main
+     * false
+     * ExecutorServiceDemo.main.submit
+     */
+    /**
+     * executorService.shutdownNow();
+     *
+     * ExecutorServiceDemo.main
+     * true
+     * java.lang.InterruptedException: sleep interrupted
+     * 	at java.lang.Thread.sleep(Native Method)
+     * 	at java.lang.Thread.sleep(Thread.java:340)
+     * 	at java.util.concurrent.TimeUnit.sleep(TimeUnit.java:386)
+     * 	at com.transformers.advanced.week04.threadpool.ExecutorServiceDemo.lambda$main$0(ExecutorServiceDemo.java:12)
+     * 	at java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:511)
+     * 	at java.util.concurrent.FutureTask.run(FutureTask.java:266)
+     * 	at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)
+     * 	at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)
+     * 	at java.lang.Thread.run(Thread.java:748)
+     */
 }
